708fe5e773ad662961621109137cb78ab7dc7c97
Megha Tyagi <meghatyagi003@gmail.com>
2020-07-10 11:06:15 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person';
 
-function App() {
-  return (
-    <div className="App">
-      <div>HEllo</div>
-    </div>
-  );
+class App extends Component{
+  state = {
+     persons : [
+    {name : 'Megha', age : 22},
+    {name : 'Mon', age : 23}
+    ]
+  }
+
+  render(){
+    return(
+      <div>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      </div>
+    )
+  }
 }
 
 export default App;
