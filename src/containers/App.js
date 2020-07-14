17495bd746b89ced4b110722b08f08360d869337
Megha Tyagi <meghatyagi003@gmail.com>
2020-07-14 13:51:27 +0530

    app-changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,95 +1,41 @@
-import React, { Component } from 'react';
+import React, {useState, Component } from 'react';
+import './App.css';
+import Char from './Char/Char'
+import Validate from './Validate/Validate';
 
-import classes from './App.css';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('[App.js] constructor');
-  }
-
+class App extends Component{
   state = {
-    persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
-  };
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
+    userName: ""
   }
+    
+  userCHangeHandler = (event) =>
+{
+    this.setState({
+      userName : event.target.value
+    })
+}
 
-  componentWillMount() {
-    console.log('[App.js] componentWillMount');
-  }
-
-  componentDidMount() {
-    console.log('[App.js] componentDidMount');
-  }
-
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
+  deleteHandler = (index) =>{
+    const del = this.state.userName.split('')
+    del.splice(index, 1);
+    const updatedlist = del.join('');
+    this.setState({
+       userName : updatedlist
     });
+  }
 
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({ persons: persons });
-  };
-
-  deletePersonHandler = personIndex => {
-    // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
-
-  render() {
-    console.log('[App.js] render');
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <Persons
-          persons={this.state.persons}
-          clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler}
-        />
-      );
-    }
-
-    return (
-      <div className={classes.App}>
-        <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
-        {persons}
+  render(){
+    const charlist = this.state.userName.split('').map((ch,index ) => {
+      return <Char character={ch} key ={index}  clicked={() => this.deleteHandler(index)}/>
+    });
+    return(
+       <div>
+        <input type="text" onChange= {this.userCHangeHandler} value={this.state.userName}/>
+      {/* <p>{this.state.userName}</p> */}
+      <Validate inputlength={this.state.userName.length}/>
+      {charlist}
       </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    )
   }
 }
 
